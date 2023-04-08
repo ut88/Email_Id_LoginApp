b444a201de311e2eefefb6780e59ff77eda14521
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-04-09 02:10:57 +0530

    useReducer hook implementation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,46 +1,73 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => {
+  if (action.type === 'user_input') {
+    return {
+      value: action.val,
+      isValid: action.val.includes('@'),
+    }
+  }
+  if (action.type === 'Input_blur')
+    return {
+      value: state.value,
+      isValid: state.value.includes('@'),
+    }
+  return { value: "", isValid: false, }
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
- useEffect(()=>{
-  console.log("EFFECT RUNNING")
 
-  return()=>{
-     console.log("Effect cleanup");
-  }
- })
-  useEffect(()=>{
-   const identifier= setTimeout(()=>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  },1000);  
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  })
+
+  useEffect(() => {
+    console.log("EFFECT RUNNING")
+
+    return () => {
+      console.log("Effect cleanup");
+    }
+  })
+  //   useEffect(()=>{
+  //    const identifier= setTimeout(()=>{
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   },1000);  
 
 
-  return ()=>{
-    console.log('cleanup')
-    clearTimeout(identifier)
-  };
-  },[enteredEmail,enteredPassword]);
+  //   return ()=>{
+  //     console.log('cleanup')
+  //     clearTimeout(identifier)
+  //   };
+  //   },[enteredEmail,enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-     }   
+    dispatchEmail({ type: 'user_input', val: event.target.value })
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
+  }
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = (event) => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: 'Input_blur' });
   };
 
   const validatePasswordHandler = () => {
@@ -49,30 +76,28 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="password">Password</label>
           <input
